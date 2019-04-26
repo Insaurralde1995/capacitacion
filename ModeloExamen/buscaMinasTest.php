7dbc85efdcffbbad8eba0c9a01eef24c56f36759
julian <insaurraldemicellijulian@gmail.com>
2019-04-26 16:25:02 -0300

    Actualizacion

--- ModeloExamen/buscaMinasTest.php
+++ ModeloExamen/buscaMinasTest.php
@@ -20,7 +20,62 @@ final class BuscaMinasTest extends TestCase
   public function testSiAgregaMina(){
   $buscaminas = new BuscaMinas (9,9);
   $this->assertTrue($buscaminas->agregarMina(1,1));
-  
   }
 
+  public function testSiAgregaMinaFueraDelTablero(){
+    $buscaminas = new BuscaMinas (9,9);
+    $this->assertFalse($buscaminas->agregarMina(11,11));
+    
+    }
+public function testSiHayMinaCerca(){
+        $buscaminas = new BuscaMinas (9,9);
+        $buscaminas->agregarMina(3,2);
+        $this->assertEquals(1,$buscaminas->jugar(3,3));
+        
+
   }
+  public function testSiNoHayMinaCerca(){
+    $buscaminas = new BuscaMinas (9,9);
+    $this->assertEquals(0,$buscaminas->jugar(3,3));
+    
+
+}
+public function testSiPerdio(){
+    $buscaminas = new BuscaMinas (9,9);
+    $buscaminas->agregarMina(3,2);
+    $this->assertEquals(0,$buscaminas->jugar(3,2));
+    
+
+}
+public function testSiJugoYNoPerdio(){
+    $buscaminas = new BuscaMinas (9,9);
+    $buscaminas->agregarMina(3,2);
+    $this->assertEquals(1,$buscaminas->jugar(3,3));
+    
+
+
+}
+public function testSiGano(){
+    $buscaminas = new BuscaMinas (9,9);
+    $buscaminas->agregarMina(3,2);
+    $buscaminas->sacarMina(3,2);
+    $this->assertEquals(1,$buscaminas->gano());
+    
+
+}
+public function testSiNoGano(){
+    $buscaminas = new BuscaMinas (9,9);
+    $buscaminas->agregarMina(3,2);
+    $this->assertEquals(0,$buscaminas->gano());
+    
+
+}
+}
+
+
+
+/*
+public function sacarMina
+public function gano
+public function terminoDeJugar
+/*
