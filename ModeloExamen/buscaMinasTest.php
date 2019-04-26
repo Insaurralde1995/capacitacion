ca847b519db79a1d147bf310be99487875a551c0
julian <insaurraldemicellijulian@gmail.com>
2019-04-26 12:53:13 -0300

    tests

--- ModeloExamen/buscaMinasTest.php
+++ ModeloExamen/buscaMinasTest.php
@@ -6,9 +6,21 @@ use PHPUnit\Framework\TestCase;
 final class BuscaMinasTest extends TestCase
 {
   /**
-   * @return Billetera
-   */
-  public function crearBilletera() {
-    $billetera = new Billetera();
-    return $billetera;
+   * @return BuscaMinas
+   */ 
+  public function crearBuscaminas() {
+    $buscaminas = new Buscaminas();
+    return $buscaminas;
+  }
+
+  public function testExistsBuscaminas() {
+    $this->assertTrue(class_exists("BuscaMinas"));
+  }
+
+  public function testSiAgregaMina(){
+  $buscaminas = new BuscaMinas (9,9);
+  $this->assertTrue($buscaminas->agregarMina(1,1));
+  
+  }
+
   }
